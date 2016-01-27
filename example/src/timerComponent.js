e8d4be0d336c0d124e03c33058ee9606236a49f9
Kurakin Alexander <kuraga333@mail.ru>
2016-01-27 20:30:43 +0300

    Example: timerComponent: FIX: Style set incorrectly

--- example/src/timerComponent.js
+++ example/src/timerComponent.js
@@ -23,7 +23,7 @@ export default class TimerComponent extends Component {
   }
 
   render() {
-    return <div style={`border: solid 1px blue; padding: 5px; height: ${this.state.count}px; overflow: hidden;`}>
+    return <div style={ { border: 'solid 1px blue', padding: '5px', height: `${this.state.count}px`, overflow: 'hidden' } }>
       I'm a child component. Do you know how long time have I existed for?
       <br />
       I know: for {String(this.state.count)} seconds.
