b607ae452d4deb64c0102674f789a63c4900801d
Kurakin Alexander <kuraga333@mail.ru>
2016-02-13 22:08:53 +0300

    Example: src/helpers/virtualDomSpreadH.js: Typo

--- example/src/helpers/virtualDomSpreadH.js
+++ example/src/helpers/virtualDomSpreadH.js
@@ -1,6 +1,6 @@
 import virtualDomH from 'virtual-dom/h';
 
-export default function h(tagName, properties, ...children) {
+export default function virtualDomSpreadH(tagName, properties, ...children) {
   // Dirty fix, see https://github.com/Matt-Esch/virtual-dom/pull/297 and https://phabricator.babeljs.io/T2034
   return Object.prototype.toString.call(children[0]) === '[object Array]'
     ? virtualDomH(tagName, properties, children[0])
